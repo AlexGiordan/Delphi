9ea5ad79f812897461ebc5d3cc7310101a548055
AlexGiordan <alex.giordan@hotmail.com>
2016-03-31 09:00:13 -0300

    Update DicasSQL.sql

--- DicasSQL.sql
+++ DicasSQL.sql
@@ -1,3 +1,9 @@
 Dicas de processo para SQL
 
-Dica 1.
+Dica 1: Como obter um IDINTEGRACAO novo padrão Uninfo.
+  Passo 1: Abra o IBExpert
+  Passo 2: Abra um novo SQL Editor.
+  Passo 3: Monte o seguinte Select:
+    Select Unf_Apenasnumeroletras(Uuid_To_Char(Gen_Uuid())) From Rdb$Database (Execute)
+    
+  obs: Se tudo estiver correto devera retornar um ID no seguinte formato por exemplo 3A10EBD7EDF24B85BC9F773B87EE29E1
